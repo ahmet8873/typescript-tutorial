476f5cacf89276ad13cf24df476a9f14f57bd33b
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-02-17 16:24:17 +0100

    variables

--- 01-intro/myVariable.ts
+++ 01-intro/myVariable.ts
@@ -11,9 +11,22 @@ console.log(greetings);
 // number
 let userId: number = 123;
 // if you type  'userId. '  you will see all the number methods
-// you dont have to type ': number'. it will automatically be number when you assign it a number value
+// type inference: you dont have to type ': number'. it will automatically be number when you assign it a number value
 
 // boolean
 let isLoggedIn: boolean = false;
+
+// any
+// it is a getaway from using typescript and not a good practise
+let hero;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
+// you have to explicitly type 'hero' to be string, like the example below
+let hero2: string;
+hero2 = getHero();
 
 export {};
