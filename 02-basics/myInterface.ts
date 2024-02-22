18520b06cb51e1d6179080f4df355dfd0026b7ff
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-02-22 23:59:06 +0100

    inheritance of interface

--- 02-basics/myInterface.ts
+++ 02-basics/myInterface.ts
@@ -13,11 +13,36 @@ interface User {
   githubUsername: string;
 }
 
+interface Admin extends User {
+  role: "admin" | "ta" | "learner";
+  username: string;
+  id: number;
+  //   githubUsername: string;
+}
 const user1: User = {
   _id: 1,
   name: "pedro",
   githubUsername: "pedro",
   id: 1,
+  startTrial() {
+    return "hello";
+  },
+  anotherMethod() {
+    return 1;
+  },
+  getCoupon(couponName: "10%") {
+    return 10;
+  },
+};
+
+// admin user
+const user2: Admin = {
+  _id: 1,
+  name: "pedro",
+  username: "pedro",
+  githubUsername: "pedro",
+  id: 1,
+  role: "admin",
   startTrial() {
     return "hello";
   },
