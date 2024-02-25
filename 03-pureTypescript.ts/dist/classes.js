4522e9362688a3a8c7c7786458f31fff9b91cbd7
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-02-25 16:13:37 +0100

    classes,public ,private properties

--- 03-pureTypescript.ts/dist/classes.js
+++ 03-pureTypescript.ts/dist/classes.js
@@ -1,11 +1,26 @@
 "use strict";
 class User {
-    constructor(name, email, age, isValid) {
+    constructor(name, email, age, isValid, _id) {
         this.name = name;
         this.email = email;
         this.age = age;
         this.isValid = isValid;
+        this._id = _id;
     }
 }
-const user1 = new User("pedro", "pedro@pedro", 20, true);
+const user1 = new User("pedro", "pedro@pedro", 20, true, "123");
 console.log(user1);
+// user1.name=1; // error
+// private
+// you can reach readonly properties , but you cant reach private properties
+// private name , #name -> these are the same thing
+// public is default
+class User2 {
+    constructor(name, email, age, isValid, _id) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.isValid = isValid;
+        this._id = _id;
+    }
+}
--- 03-pureTypescript.ts/src/classes.ts
+++ 03-pureTypescript.ts/src/classes.ts
@@ -3,13 +3,39 @@ class User {
   email: string;
   age: number;
   isValid: boolean;
-  constructor(name: string, email: string, age: number, isValid: boolean) {
+  private readonly _id: string;
+  constructor(
+    name: string,
+    email: string,
+    age: number,
+    isValid: boolean,
+    _id: string
+  ) {
     this.name = name;
     this.email = email;
     this.age = age;
     this.isValid = isValid;
+    this._id = _id;
   }
 }
 
-const user1 = new User("pedro", "pedro@pedro", 20, true);
+const user1 = new User("pedro", "pedro@pedro", 20, true, "123");
 console.log(user1);
+
+// user1.name=1; // error
+
+// private
+// you can reach readonly properties , but you cant reach private properties
+// private name , #name -> these are the same thing
+
+// public is default
+
+class User2 {
+  constructor(
+    public name: string,
+    public email: string,
+    public age: number,
+    public isValid: boolean,
+    private readonly _id: string
+  ) {}
+}
