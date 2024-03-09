ab7c651e6e063ec49ce8d51d78b4a231d787aa2f
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-03-09 11:58:40 +0100

    generics-2

--- 03-pureTypescript.ts/dist/myGenerics.js
+++ 03-pureTypescript.ts/dist/myGenerics.js
@@ -1,6 +1,7 @@
 "use strict";
 const score = [1, 2, 3, 4, 5];
 const score2 = [1, 2, 3, 4, 5];
+// Array<Type> is the same as Type[]
 const names = ["pedro", "juan"];
 function identityOne(val) {
     return val;
@@ -17,3 +18,21 @@ function identityFour(val) {
     return val;
 }
 identityFour({ color: "red", volume: 10 });
+//--------------------------------------
+//if I return T from function it means I will return one of the value in this array
+function getSearchProducts(products) {
+    //return 3; // error
+    return products[3];
+}
+// arrow function
+const getSearchProducts2 = (products) => {
+    return products[3];
+};
+console.log(getSearchProducts([1, 2, 3, 4, 5])); // 4
+console.log(getSearchProducts2([" ali ", " pedro ", " juan ", " john", "jack"])); // john
+// default value example
+const getSearchProducts3 = (products, index = 0) => {
+    return products[index];
+};
+console.log(getSearchProducts3([1, 2, 3, 4, 5]));
+console.log(getSearchProducts3([" ali ", " pedro ", " juan ", " john", "jack"], 3));
--- 03-pureTypescript.ts/src/myGenerics.ts
+++ 03-pureTypescript.ts/src/myGenerics.ts
@@ -1,5 +1,6 @@
 const score: Array<number> = [1, 2, 3, 4, 5];
 const score2: number[] = [1, 2, 3, 4, 5];
+// Array<Type> is the same as Type[]
 
 const names: Array<string> = ["pedro", "juan"];
 
@@ -29,3 +30,33 @@ interface bootles {
   volume: number;
 }
 identityFour({ color: "red", volume: 10 });
+
+//--------------------------------------
+
+//if I return T from function it means I will return one of the value in this array
+function getSearchProducts<T>(products: T[]): T {
+  //return 3; // error
+  return products[3];
+}
+
+// arrow function
+
+const getSearchProducts2 = <T>(products: T[]): T => {
+  return products[3];
+};
+
+console.log(getSearchProducts([1, 2, 3, 4, 5])); // 4
+
+console.log(
+  getSearchProducts2([" ali ", " pedro ", " juan ", " john", "jack"])
+); // john
+
+// default value example
+const getSearchProducts3 = <T>(products: T[], index: number = 0): T => {
+  return products[index];
+};
+
+console.log(getSearchProducts3([1, 2, 3, 4, 5]));
+console.log(
+  getSearchProducts3([" ali ", " pedro ", " juan ", " john", "jack"], 3)
+);
